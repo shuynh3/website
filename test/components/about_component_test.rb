c76c9c73821e074e8b8ed40a42dc11a55f2d3fad
Sonny Huynh <sonny.huynh@strongmind.com>
2022-07-27 22:47:03 -0700

    Add about section and fix some styling

---
+++ test/components/about_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class AboutComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(AboutComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
