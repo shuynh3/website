94dc93fc759bc17b5b816c6c8e18f471687cb3d2
Sonny Huynh <sonny.huynh@strongmind.com>
2022-08-14 20:06:54 -0700

    add infrastructure mobile component

---
+++ app/components/tech/infrastructure_mobile_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Tech::InfrastructureMobileComponent < ViewComponent::Base
+
+end
---
+++ test/components/tech/infrastructure_mobile_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Tech::InfrastructureMobileComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Tech::InfrastructureMobileComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
