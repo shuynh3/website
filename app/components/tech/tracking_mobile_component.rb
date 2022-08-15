9d6029bee2b82131080a086e8cd584a78d3c7959
Sonny Huynh <sonny.huynh@strongmind.com>
2022-08-14 20:12:19 -0700

    Add tracking component

---
+++ app/components/tech/tracking_mobile_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Tech::TrackingMobileComponent < ViewComponent::Base
+
+end
---
+++ test/components/tech/tracking_mobile_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Tech::TrackingMobileComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Tech::TrackingMobileComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
